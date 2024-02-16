8e074e671b590dec183d2c99dc67e28ec4c53e53
Daniel Taejoon Kim <kimdt@purdue.edu>
2024-02-16 14:33:17 -0500

    coded text

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -2,19 +2,30 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<char> intString({ 'V', 'F', 'X', 'B', 'L', 'I', 'T', 'Z', 'J', 'R', 'P', 'H', 'D', 'K', 'N', 'O', 'W', 'S', 'G', 'U', 'Y', 'Q', 'M', 'A', 'C', 'E' }); 
+    string text;
+    cout << "Input text to cypher: ";  
+    getline(cin, text); 
+    cout << "Encoded message: "; 
+    int n;
+    n = 0;
+    for (char n : text) { 
+        if (90 > n && n >= 65) {
+            cout << intString[n - 65];
+        }
+        if (122 > n && n >= 97) {
+            cout << intString[n - 97];
+            int upperCaseLetter(n - 32);
+        }
+        else {
+            cout << n; 
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
